1cde3dc9e8b6d210dfb70316c1b1377786bd09c0
Rehoboth <rehoboth.iyasele@babbangona.com>
2022-03-01 16:25:00 +0100

    sending response

--- nodejs-basics/app.js
+++ nodejs-basics/app.js
@@ -1,9 +1,27 @@
 const http =  require('http');
+const { title } = require('process');
 
 
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    console.log(req.url, req.method, req.headers);
+    //process.exit()
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First NodeJS Page</title></head>');
+    res.write('<body><h1>Hello from my Nodejs server !</h1></body>');
+    res.write('</html>');
+    res.end();
+
 });
 
 server.listen(3000);
