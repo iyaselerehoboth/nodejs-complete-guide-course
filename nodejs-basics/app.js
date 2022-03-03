31ab91b6a431ff412d13edc353c2ba658002b5ee
Iyasele Rehoboth <rehobothi@yahoo.com>
2022-03-03 17:34:04 +0100

    Switched to routes and using exports

--- nodejs-basics/app.js
+++ nodejs-basics/app.js
@@ -1,55 +1,9 @@
 const http =  require('http');
-const fs = require('fs');
-const { title } = require('process');
 
+const routes = require('./routes')
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+console.log(routes.someText);
 
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-
-        /* Create a listener that fires whenever a new chunk of data 
-           is transmitted in the current ongoing request transfer 
-           (nodejs transfers the data in chunks and not necessarily at once depending on the size).
-        */
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-
-        /*
-            On end of the data transfer, we then create a buffer from the body array.
-        */
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-
-    console.log(req.url, req.method, req.headers);
-    //process.exit()
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First NodeJS Page</title></head>');
-    res.write('<body><h1>Hello from my Nodejs server !</h1></body>');
-    res.write('</html>');
-    res.end();
-
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
---
+++ nodejs-basics/routes.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+    
+        /* Create a listener that fires whenever a new chunk of data 
+           is transmitted in the current ongoing request transfer 
+           (nodejs transfers the data in chunks and not necessarily at once depending on the size).
+        */
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+    
+        /*
+            On end of the data transfer, we then create a buffer from the body array.
+        */
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, err => {   
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    
+    }
+    
+    //console.log(req.url, req.method, req.headers);
+    //process.exit()
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First NodeJS Page</title></head>');
+    res.write('<body><h1>Hello from my Nodejs server !</h1></body>');
+    res.write('</html>');
+    res.end();
+};
+
+/*
+- A method of requesting just one function.
+
+module.exports = requestHandler;
+*/
+
+
+/*
+- Method A of exporting multiple functions.
+
+module.exports = {
+    handler: requestHandler,
+    someText: 'Some text or so'
+};
+*/
+
+//Method B of exporting multiple functions. Or you can remove 'module' and  just call it exports.handler etc...
+module.exports.handler = requestHandler;
+module.exports.someText = 'This is some hard coded text here';
