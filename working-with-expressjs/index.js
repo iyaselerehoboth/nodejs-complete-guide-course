21231770c2d11427cda37ce5eaf7363697696d2c
Iyasele Rehoboth <rehobothi@yahoo.com>
2022-08-30 20:59:37 +0100

    use express to write cleaner routes

--- working-with-expressjs/index.js
+++ working-with-expressjs/index.js
@@ -1,9 +1,19 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+/**
+ * 'use' method allows us to create a middleware, 
+ * that would be executed for every incoming request.
+ */
+app.use((req, res, next) => {
+    console.log("In the middleware");
+    next(); //Allows the request to continue on to the next middleware in line
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log("In another middleware");
+    res.send('<h1> Hello from ROBO ! </h1>');
+});
+
+app.listen(3000);
