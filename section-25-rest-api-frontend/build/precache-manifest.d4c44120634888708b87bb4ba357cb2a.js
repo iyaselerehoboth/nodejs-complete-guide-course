c3cf55ef8fb439e98043adce96163894faf93aba
Iyasele Rehoboth <rehobothi@yahoo.com>
2023-02-14 13:03:59 +0100

    setup frontend files

---
+++ section-25-rest-api-frontend/build/precache-manifest.d4c44120634888708b87bb4ba357cb2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6b22db6a74d7cd55611",
+    "url": "/static/css/main.7500e9f1.chunk.css"
+  },
+  {
+    "revision": "a6b22db6a74d7cd55611",
+    "url": "/static/js/main.da948799.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d66c9a2f67da211429f",
+    "url": "/static/js/2.5ce8b470.chunk.js"
+  },
+  {
+    "revision": "d5285e94368530eca5fa50b80bf8acf9",
+    "url": "/index.html"
+  }
+];
