f6d1c4d0a9bd4e2bc844e056e09e84e4af216a2f
Iyasele Rehoboth <rehobothi@yahoo.com>
2023-02-20 22:35:45 +0100

    about to add mongoose

--- section-25-rest-api-backend/routes/feed.js
+++ section-25-rest-api-backend/routes/feed.js
@@ -10,8 +10,12 @@ router.get('/posts', feedController.getPosts);
 
 //POST base_url/feed/post
 router.post('/post', [
-    body('title').trim().isLength({min: 5}),
-    body('content').trim().isLength({min: 5})
+    body('title')
+        .trim()
+        .isLength({min: 5}),
+    body('content')
+        .trim()
+        .isLength({min: 5})
 ], feedController.createPost);
 
 module.exports = router
